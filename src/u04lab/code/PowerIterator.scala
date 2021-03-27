1e8cf0bb6b766e7cbf22fc509acdeb98b3dae81b
nic0lasfara <nicolas.farabegoli@gmail.com>
2021-03-27 13:09:36 +0100

    Simplified next() function in iterator

--- src/u04lab/code/PowerIterator.scala
+++ src/u04lab/code/PowerIterator.scala
@@ -2,11 +2,12 @@ package u04lab.code
 
 import Optionals._
 import Lists._
-import u04lab.code.Lists.List.Cons
+import u04lab.code.Lists.List.reverse
+import u04lab.code.Optionals.Option.ifPresent
+import u04lab.code.Streams.Stream.{drop, head}
 import u04lab.code.Streams._
 
 import java.util.Random
-import scala.annotation.tailrec
 
 
 trait PowerIterator[A] {
@@ -20,27 +21,20 @@ object PowerIterator {
   def fromList[A](list: List[A]): PowerIterator[A] = PowerIteratorImpl(List.stream(list))
   def randomBooleans(size: Int): PowerIterator[Boolean] = PowerIteratorImpl(Stream.take(Stream.generate(new Random().nextBoolean()))(size))
 
-  private case class PowerIteratorImpl[A](private val stream: Stream[A]) extends PowerIterator[A] {
+  private case class PowerIteratorImpl[A](private var stream: Stream[A]) extends PowerIterator[A] {
 
     private var pastList: List[A] = List.Nil()
-    private var nextIndex = -1
 
     override def next(): Option[A] = {
-      @tailrec
-      def _next(stream: Stream[A], currNext: Int): Option[A] = stream match {
-        case Stream.Cons(_, tail) if currNext > 0 => _next(tail(), currNext - 1)
-        case Stream.Cons(head, _) if currNext == 0 =>
-          pastList = List.append(pastList, Cons(head(), List.Nil()))
-          Option.of(head())
-        case _ => Option.empty
-      }
-      nextIndex += 1
-      _next(stream, nextIndex)
+      val nextElem = head(stream)
+      ifPresent(nextElem)(e => pastList = List.Cons(e, pastList))
+      stream = drop(stream)(1)
+      nextElem
     }
 
-    override def allSoFar(): List[A] = this.pastList
+    override def allSoFar(): List[A] = reverse(this.pastList)
 
-    override def reversed(): PowerIterator[A] = PowerIteratorImpl(List.stream(List.reverse(pastList)))
+    override def reversed(): PowerIterator[A] = PowerIteratorImpl(List.stream(pastList))
   }
 }
 
